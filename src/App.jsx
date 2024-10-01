e57f6f274e6107e4eb3fc1636330bcfd9f023ba1
Alisa Ermel <alisa.ermel@myy.haaga-helia.fi>
2024-10-01 12:31:51 +0300

    Exercise enhanced

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import Todolist from './components/Todolist'
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Home from './components/Home'
 
 function App() {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <Container maxWidth="md">
       <AppBar position="static">
@@ -15,7 +24,12 @@ function App() {
           </Typography>
         </Toolbar>
       </AppBar>
-      <Todolist />
+      <Tabs value={value} onChange={handleChange} centered>
+        <Tab label="Home" />
+        <Tab label="Todos" />
+      </Tabs>
+      {value === 0 && <Home />}
+      {value === 1 && <Todolist />}
       <CssBaseline />
     </Container>
   )
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import Typography from '@mui/material/Typography';
+
+const Home = () => {
+    return (
+        <div style={{ padding: '20px' }}>
+            <Typography variant="h4">Welcome to My Todo App!</Typography>
+        </div>
+    );
+}
+
+export default Home;
